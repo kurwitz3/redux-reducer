65d72226eda29357a412b4e87f0afe3b946fbe91
kurwitz3 <kurwitz3@aol.com>
2021-03-08 20:37:39 -0500

    commit tree ea2dd1ef51a13daccb72db0c29bf68bcc39442c5

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+
+            return { ...state, friends : [...state.friends, action.friend] }
+                
+                
+            
+            case 'REMOVE_FRIEND':
+
+                return {
+                    ...state,
+                    friends: state.friends.filter(item => action.id !== item.id)
+                }
+            default: 
+            return state
+    }
 }
